6bd5977e7079db24b8ca1bea7945b4994108715a
BismaIhsan <68103183+BismaIhsan@users.noreply.github.com>
2020-10-23 21:51:10 +0700

    Add files via upload

---
+++ untuk_hafidz.py
@@ -0,0 +1,39 @@
+print("halo, dengan siapa disana?")
+print("")
+nama=input("nama kamu siapa?")
+nama_asli=("hafidz")
+
+if nama==nama_asli:
+	print("halo",nama,"!")
+	
+else:
+	print("boong kan kamu, gw tau kmu hafidz")
+	
+print("")
+sayangga=input("kamu sayang ga sama aku?, iya / ga")
+
+if sayangga==("iya"):
+	print("aku sayang juga sama kamu :)")
+	print("")
+	bangetga=input("sayang nya pake banget ga?, iya/ga")
+	
+	if bangetga==("iya"):
+		print("aku juga sayang banget sama kamu ")
+	else:
+		print("yaudah deh, aku sayang sama kamu juga")
+		
+else:
+		print("huhuhu sedih :(")
+	
+		
+			
+boba=input("kamu mau beliin aku boba ga?, mau/ga")
+
+if boba==("mau"):
+		print("makasih sayannggg, muach")
+		
+else:
+		print("yaudah deh")
+							
+print("udah dulu ya, aku pamit")
+		
